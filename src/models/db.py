6a7aa9366db9155b5ac879293d78d90bd58f2f39
petrCher <88943157+petrCher@users.noreply.github.com>
2026-02-23 23:23:39 +0300

    ready models

--- src/models/db.py
+++ src/models/db.py
@@ -22,9 +22,27 @@ class Holidays(BaseDBModel):
     till_date: Mapped[str] = mapped_column(String, nullable=False, default="null")
 
 
-class MetInfo(BaseDBModel):
-    __tablename__ = "met_info"
+class MeetInfo(BaseDBModel):
+    __tablename__ = "meet_info"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     first_user_id: Mapped[int] = mapped_column(ForeignKey("user_info.id"))
     second_user_id: Mapped[int] = mapped_column(ForeignKey("user_info.id"))
-    date: Mapped[str]
+
+
+class Username(BaseDBModel):
+    __tablename__ = "tg_usernames"
+    id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("user_info.id"), primary_key=True
+    )
+    username: Mapped[str] = mapped_column(String, nullable=False)
+
+
+class BanList(BaseDBModel):
+    __tablename__ = "ban_list"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    banned_user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user_info.id"))
+    ban_status: Mapped[int] = mapped_column(Integer, default=1)
+    date_of_ban: Mapped[str] = mapped_column(default="null")
+    comment_to_ban: Mapped[str] = mapped_column(String(500))
+    date_of_unban: Mapped[str] = mapped_column(default="null")
+    comment_to_unban: Mapped[str] = mapped_column(String(500), default="null")
