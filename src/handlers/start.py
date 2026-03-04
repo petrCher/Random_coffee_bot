5083dd9c0182988106e1d0a881697b9ae6d7b41a
petrCher <88943157+petrCher@users.noreply.github.com>
2026-03-04 19:43:51 +0300

    minor fixes

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,12 +1,13 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from src.utils.answers import Answers
 
 router = Router()
-
+answer = Answers()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
     await message.answer(
-        f"Я бот. Приятно познакомиться, {message.from_user.first_name}!"
+        f"Привет, {message.from_user.first_name}!\n{answer.start_message}"
     )
