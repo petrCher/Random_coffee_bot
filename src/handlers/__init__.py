fba8686af09c6186d5216d0c9ca52658a6279085
petrCher <88943157+petrCher@users.noreply.github.com>
2026-03-05 22:57:12 +0300

    not ready fixes yet

---
+++ src/handlers/__init__.py
@@ -0,0 +1,11 @@
+from aiogram import Router
+
+from src.handlers import registration, admin, menu, text
+
+def setup_routers() -> Router:
+    root=Router
+    root.include_router(admin.router)
+    root.include_router(registration.router)
+    root.include_router(menu.router)
+    root.include_router(text.router)
+    return root
