b1f09da8c80782cb9d82762f0932dd2b668382cd
petrCher <88943157+petrCher@users.noreply.github.com>
2026-04-05 21:27:34 +0300

    ready registration

--- src/handlers/registration.py
+++ src/handlers/registration.py
@@ -1,18 +1,23 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import CallbackQuery, Message
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from src.utils.answers import Answers
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.models.db import Users
+from src.models.db import Users, Username
 from sqlalchemy import select
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 router = Router()
 answer = Answers()
 
 class RegFSM(StatesGroup):
     waiting_full_name = State()
+    waiting_birthday = State()
+    waiting_info = State()
+    waiting_review = State()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message, state: FSMContext, session: AsyncSession)-> None:
@@ -27,15 +32,19 @@ async def cmd_start(message: Message, state: FSMContext, session: AsyncSession)-
             f"Привет, {username}!\nТы уже зарегистрирован"
         )
         return
-    await Users.create(session=session, tg_id=user_id)
+    user = await Users.create(session=session, tg_id=user_id)
+    await Username.create(session=session, id=user.id, username=username)
     await state.set_state(RegFSM.waiting_full_name)
     await message.answer(
-        f"Привет, {username}!\nВведи свое ФИО для регистрации"
+        f"Привет, {username}!"
     )
+    await message.answer("Введите свое ФИО для регистрации")
     return
 
 @router.message(RegFSM.waiting_full_name)
-async def process_full_name(message: Message, state: FSMContext, session: AsyncSession):
+async def get_full_name(message: Message, state: FSMContext, session: AsyncSession):
+    data = await state.get_data()
+    is_edit = data.get("edit_field") == "name"
     if not message.text or not message.from_user:
         await message.answer("Пожалуйста, введите корректное ФИО")
         return
@@ -44,6 +53,78 @@ async def process_full_name(message: Message, state: FSMContext, session: AsyncS
     user = await Users.query(session=session).filter(Users.tg_id == user_id).one_or_none()
     id=user.id
     await Users.update(id=id, session=session, name=full_name)
-    await message.answer(
-        f"Твое ФИО: <b>{full_name}</b>", parse_mode="HTML"
-    )
+    if is_edit:
+        await message.answer("ФИО успешно обновлено!")
+        await state.clear()
+        await show_all_info(id, message, session)
+    else:
+        await state.set_state(RegFSM.waiting_birthday)
+        await message.answer("Введите свою дату рождения в формате dd.mm.yyyy")
+
+@router.message(RegFSM.waiting_birthday)
+async def get_birthday(message: Message, state: FSMContext, session: AsyncSession):
+    data = await state.get_data()
+    is_edit = data.get("edit_field") == "birthday"
+    if not message.text or not message.from_user:
+        await message.answer("Пожалуйста, введите дату рождения")
+        return
+    birthday=message.text
+    user_id=message.from_user.id
+    user = await Users.query(session=session).filter(Users.tg_id == user_id).one_or_none()
+    id=user.id
+    await Users.update(id=id, session=session, birthday=birthday)
+    if is_edit:
+        await message.answer("День рождения успешно обновлен!")
+        await state.clear()
+        await show_all_info(id, message, session)
+    else:
+        await state.set_state(RegFSM.waiting_info)
+        await message.answer("Введите информацию о себе")
+
+@router.message(RegFSM.waiting_info)
+async def get_info(message: Message, state: FSMContext, session: AsyncSession):
+    data = await state.get_data()
+    is_edit = data.get("edit_field") == "about"
+    if not message.text or not message.from_user:
+        await message.answer("Пожалуйста, введите информацию о себе")
+        return
+    info=message.text
+    user_id=message.from_user.id
+    user = await Users.query(session=session).filter(Users.tg_id == user_id).one_or_none()
+    id=user.id
+    await Users.update(id=id, session=session, about=info)
+    if is_edit:
+        await message.answer("Информация о себе успешно обновлена!")
+    await state.clear()
+    await show_all_info(id, message, session)
+    
+async def show_all_info(id: int, message: Message, session: AsyncSession):
+    user = await Users.query(session=session).filter(Users.id==id).one_or_none()
+    text = f"Твоя карточка:\nФИО - <b>{user.name}</b>\nДата рождения - <b>{user.birthday}</b>\nИнформация о себе - <b>{user.about}</b>"
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Изменить ФИО", callback_data="edit:name")
+    builder.button(text="Изменить ДР", callback_data="edit:birthday")
+    builder.button(text="Изменить Инфо о себе", callback_data="edit:about")
+    builder.button(text="Всё верно, оставлю", callback_data="edit:done")
+    builder.adjust(1)
+    await message.answer(text, reply_markup=builder.as_markup(), parse_mode="HTML")
+
+@router.callback_query(F.data.startswith("edit"))
+async def edit_profile(callback: CallbackQuery, state: FSMContext, session: AsyncSession):
+    action = callback.data.split(":")[1]
+    if action == "done":
+        await callback.message.edit_text("Отлично! Данные сохранены.")
+        await state.clear()
+        await callback.answer()
+        return
+    await state.update_data(edit_field=action)
+    if action == "name":
+        await callback.message.edit_text("Введите новое ФИО:")
+        await state.set_state(RegFSM.waiting_full_name)
+    elif action == "birthday":
+        await callback.message.edit_text("Введите новую дату рождения dd.mm.yyyy:")
+        await state.set_state(RegFSM.waiting_birthday)
+    elif action == "about":
+        await callback.message.edit_text("Введите новую информацию о себе:")
+        await state.set_state(RegFSM.waiting_info)
+    await callback.answer()
