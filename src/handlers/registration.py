52fbe1d94c5c022d28964de5ec9586f14085bf72
petrCher <88943157+petrCher@users.noreply.github.com>
2026-04-05 19:20:23 +0300

    added fio

--- src/handlers/registration.py
+++ src/handlers/registration.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from src.utils.answers import Answers
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,6 +10,9 @@ from sqlalchemy import select
 
 router = Router()
 answer = Answers()
+
+class RegFSM(StatesGroup):
+    waiting_full_name = State()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message, state: FSMContext, session: AsyncSession)-> None:
@@ -24,7 +28,22 @@ async def cmd_start(message: Message, state: FSMContext, session: AsyncSession)-
         )
         return
     await Users.create(session=session, tg_id=user_id)
+    await state.set_state(RegFSM.waiting_full_name)
     await message.answer(
-        f"Привет, {username}!\n{Answers.start_message}"
+        f"Привет, {username}!\nВведи свое ФИО для регистрации"
     )
     return
+
+@router.message(RegFSM.waiting_full_name)
+async def process_full_name(message: Message, state: FSMContext, session: AsyncSession):
+    if not message.text or not message.from_user:
+        await message.answer("Пожалуйста, введите корректное ФИО")
+        return
+    full_name=message.text
+    user_id=message.from_user.id
+    user = await Users.query(session=session).filter(Users.tg_id == user_id).one_or_none()
+    id=user.id
+    await Users.update(id=id, session=session, name=full_name)
+    await message.answer(
+        f"Твое ФИО: <b>{full_name}</b>", parse_mode="HTML"
+    )
